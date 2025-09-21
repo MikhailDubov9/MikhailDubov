d0f3d7187e0d3da275665f027572acd060f42dce
dubov.mm <dubov.mm@phystech.edu>
2025-09-21 21:09:14 +0300

    Add java class MainClass

---
+++ src/main/java/com/mipt/mikhaildubov/MainClass.java
@@ -0,0 +1,12 @@
+package com.mipt.mikhaildubov;
+
+public class MainClass {
+  private int justInt;
+  private String justString;
+  protected static double justDouble;
+  public final long justLong = 52;
+
+  public static void main(String[] args) {
+
+  }
+}

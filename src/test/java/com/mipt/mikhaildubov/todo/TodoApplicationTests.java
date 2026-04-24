69ed462555433352f3a856cf78a2c06f529b32c9
dubov.mm <dubov.mm@phystech.edu>
2026-04-25 00:56:09 +0300

    Small fixes

--- src/test/java/com/mipt/mikhaildubov/todo/TodoApplicationTests.java
+++ src/test/java/com/mipt/mikhaildubov/todo/TodoApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import com.mipt.mikhaildubov.todo.service.TaskService;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class TodoApplicationTests {
 
   @Autowired
